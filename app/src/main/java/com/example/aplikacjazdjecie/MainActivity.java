57ae46e1f3ce85a41761a62aa73446134282db9b
ogkamilek <kamil.kozlowski.hel@gmail.com>
2022-10-17 12:08:31 +0200

    Testowanie zmian wlasciwosci obrazka

--- app/src/main/java/com/example/aplikacjazdjecie/MainActivity.java
+++ app/src/main/java/com/example/aplikacjazdjecie/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.aplikacjazdjecie;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void klikTest(View view) {
+        ImageView obraz = (findViewById(R.id.obrazek));
+
+        // max wys=700 max szer=1100
+        obraz.getLayoutParams().height = 300;
+        obraz.getLayoutParams().width = 500;
+        obraz.setScaleType(ImageView.ScaleType.FIT_XY);
+
+        obraz.setRotation(obraz.getRotation()+90);
+
+        obraz.setImageAlpha(50);
+
+
+        final int semiTransparentGrey = Color.argb(155, 185, 185, 185);
+        obraz.setColorFilter(semiTransparentGrey, PorterDuff.Mode.SRC_ATOP);
+
+
+
+
     }
 }
