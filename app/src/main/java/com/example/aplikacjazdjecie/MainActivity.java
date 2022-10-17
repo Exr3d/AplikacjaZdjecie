d39adab4a44300454acc576823b29f93fe4db182
ogkamilek <kamil.kozlowski.hel@gmail.com>
2022-10-17 13:25:05 +0200

    Naprawa dzialania filtru kolorow

--- app/src/main/java/com/example/aplikacjazdjecie/MainActivity.java
+++ app/src/main/java/com/example/aplikacjazdjecie/MainActivity.java
@@ -7,33 +7,41 @@ import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+ImageView obraz;
+TextView testTXT;
+int wys=100;
+int kolorR=255;
+int kolorG=255;
+int kolorB=255;
+int alph=255;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ImageView obraz = (findViewById(R.id.obrazek));
+        obraz = (findViewById(R.id.obrazek));
+        testTXT = (findViewById(R.id.testTXT));
 
-        // max wys=700 max szer=1100
-        obraz.getLayoutParams().height = 300;
-        obraz.getLayoutParams().width = 500;
-        obraz.setScaleType(ImageView.ScaleType.FIT_XY);
+        //obraz.getLayoutParams().height = 100;
+        //obraz.getLayoutParams().width = 100;
+        //obraz.setScaleType(ImageView.ScaleType.FIT_XY);
 
-        obraz.setRotation(obraz.getRotation()+90);
+        //obraz.setRotation(obraz.getRotation()+90);
 
         //obraz.setImageAlpha(50);
 
 
-        final int semiTransparentGrey = Color.argb(155, 255, 110, 0);
+        final int semiTransparentGrey = Color.argb(alph, kolorR, kolorG, kolorB);
         obraz.setColorFilter(semiTransparentGrey, PorterDuff.Mode.SRC_ATOP);
 
 
         SeekBar seekWys = (findViewById(R.id.seekWys));
-        SeekBar seekSzer = (findViewById(R.id.seekWys2));
         SeekBar seekPrzez = (findViewById(R.id.seekWys3));
         SeekBar seekFiltrR = (findViewById(R.id.seekWys4));
         SeekBar seekFiltrG = (findViewById(R.id.seekWys5));
@@ -42,6 +50,92 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
 
+
+                wys = i;
+                LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(wys, wys);
+                //obraz.setLayoutParams(params);
+
+                //obraz.setScaleType(ImageView.ScaleType.FIT_XY);
+                //obraz.getLayoutParams().height = i;
+                //obraz.getLayoutParams().width = i;
+                //obraz.setScaleType(ImageView.ScaleType.FIT_XY);
+                testTXT.setText("fff"+i);
+                obraz.setScaleX(wys/10);
+                obraz.setScaleY(wys/10);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        seekPrzez.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                obraz.setImageAlpha(i);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        seekFiltrR.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                kolorR = i;
+                int semiTransparentGrey = Color.argb(alph, kolorR, kolorG, kolorB);
+                obraz.setColorFilter(semiTransparentGrey, PorterDuff.Mode.SRC_ATOP);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        seekFiltrG.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                kolorG = i;
+                int semiTransparentGrey = Color.argb(alph, kolorR, kolorG, kolorB);
+                obraz.setColorFilter(semiTransparentGrey, PorterDuff.Mode.SRC_ATOP);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        seekFiltrB.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                kolorB = i;
+                int semiTransparentGrey = Color.rgb(kolorR, kolorG, kolorB);
+                obraz.setColorFilter(semiTransparentGrey, PorterDuff.Mode.SRC_ATOP);
             }
 
             @Override
